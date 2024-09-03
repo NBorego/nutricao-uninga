d08a86ea45020c98dd9a2cc022aa097d9ddda369
Nicolas Borego <129863873+NBorego@users.noreply.github.com>
2024-09-03 13:26:48 -0300

    feature/model agendamento criado

--- agendamento/models.py
+++ agendamento/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from login.models import Cliente, Nutricionista
 
-# Create your models here.
+class Agendamento(models.Model):
+    id_cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    id_nutricionista = models.ForeignKey(Nutricionista, on_delete=models.CASCADE)
+    data = models.DateField()
+    horario = models.TimeField()
+    status = models.CharField(max_length=20, choices=[
+        ('Pendente', 'Pendente'),
+        ('Confirmado', 'Confirmado'),
+        ('Cancelado', 'Cancelado')
+    ])
+    tem_patologia = models.BooleanField()
+
+    class Meta:
+        verbose_name = "Agendamento"
+        verbose_name_plural = "Agendamentos"
+
+    def __str__(self):
+        return self.nome
